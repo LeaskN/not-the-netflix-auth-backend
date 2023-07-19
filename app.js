baaba9ef5644bfb38e85cd50c904bb9304a87867
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-19 19:24:37 -0400

    feat: handling cors

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ const auth = require("./auth");
 
 dbConnect();
 
+// Curb CORS Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
 app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
